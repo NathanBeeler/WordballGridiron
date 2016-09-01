841e54513b562ccd0740ea9c5beb06a41732937e
Nathan Beeler <n_adrian_b@hotmail.com>
2016-09-01 14:57:05 -0700

    Initial commit

---
+++ Assets/Scripts/GameLevelController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLevelController : MonoBehaviour {
+
+  public LetterTileController letterTilePrefab;
+
+  public void CreateLetterTile (char letterType)
+  {
+    LetterTileController lt = Instantiate(letterTilePrefab) as LetterTileController;
+    Sprite letter = Resources.Load<Sprite>("Sprites/Letter" + letterType);
+    lt.SetLetterSprite(letter);
+    lt.typeOfLetter = letterType;
+  }
+
+  void Start()
+  {
+    CreateLetterTile('B');
+  }
+}
---
+++ Assets/Scripts/LetterTileController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LetterTileController : MonoBehaviour {
+
+  public char typeOfLetter;
+  public SpriteRenderer letter;
+
+  public void SetLetterSprite (Sprite chosenLetter)
+  {
+    letter.sprite = chosenLetter;
+  }
+}
